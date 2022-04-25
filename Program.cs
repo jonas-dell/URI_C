4daa6cc01fe3d3861683db8720ce63d39dd8cb00
unknown <jonaseal17@gmail.com>
2022-04-25 20:09:36 -0300

    Exercicio 1003

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Exer1003
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int A = int.Parse(Console.ReadLine());
+            int B = int.Parse(Console.ReadLine());
+
+            int SOMA = A + B;
+            Console.WriteLine("SOMA = " + SOMA);
+        }
+    }
+}
